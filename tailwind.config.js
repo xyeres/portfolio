4707c2192b066d679aff2c8133749f0af6da40f7
Michael Carr <xyeres@me.com>
2022-04-29 18:09:02 -0700

    Configure tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'teal-primary':'rgb(87,120,135)',
+        'blue-primary': '#3BA0FD',
+        'purple-primary': '#BC2FFF',
+      },
+    },
+  },
+  plugins: [],
+}
