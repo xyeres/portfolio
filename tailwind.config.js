3fd093498c9252c4e4be9c17c3b30374684ac67b
Michael Carr <xyeres@me.com>
2022-05-04 12:42:31 -0700

    Add breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
@@ -5,6 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        ...defaultTheme.screens,
+        '900px': '900px',
+        '1000px': '1000px',
+        '1400px': '1400px',
+        '1600px': '1600px',
+      },
       colors: {
         'teal-primary':'rgb(87,120,135)',
         'blue-primary': '#3BA0FD',
