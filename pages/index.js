4a439c58a04430cbfae577f6ebd824dbdd41c2e1
Michael Carr <xyeres@me.com>
2022-05-03 21:20:15 -0700

    Tighten styles

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { Card } from './../components/Card';
 import Image from "next/image";
 import Link from "next/link";
 
@@ -5,30 +6,53 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro'
 import Icon from "../components/Icon";
 
+import {useRef, useState} from 'react'
+
 export default function Home() {
 
+  const [tooManyClicks, settooManyClicks] = useState(false)
+
+  const clicks = useRef(0)
+  const handleTooManyClicks = () => {
+    clicks.current++
+    console.log(clicks)
+    if (clicks.current > 5) {
+      settooManyClicks(true)
+    }
+    if (clicks.current > 9) {
+      settooManyClicks(false)
+    }
+  }
 
   return (
-    <>
-      <header className="pt-12 px-7 bg-gradient-to-b from-[#fffcdf] bg-white bg-no-repeat bg-cover text-black flex flex-col items-center">
-        <div className="rounded-xl border-solid border-b-8 border-b-slate-600">
+    <div className='max-w-xl m-auto'>
+      <header className="pt-12 px-7 text-black flex flex-col items-center">
+        <div onClick={handleTooManyClicks} className="relative active:scale-90 flex items-center justify-center group avatar-container">
           <Image
             alt="Michael Carr"
             src={'/avatar.jpg'}
-            width={180}
-            height={180}
-            className="rounded-lg"
+            width={120}
+            height={120}
+            className="avatar"
           />
+        {tooManyClicks && <>
+            <span className='sqThing'></span>
+            <p className='absolute bg-pink-200 -rotate-6 w-24 shadow flex -right-14 text-xs p-2 top-4 font-semibold flex-col gap-2 text-center items-center justify-center'>
+              <FontAwesomeIcon size='2x' icon={solid("bomb")} />
+              Whoa that was a lot
+            </p>
+          </>}
+
         </div>
         <h1 className="text-black text-2xl font-black mt-4">Michael Carr</h1>
         <h2 className="text-gray-600 text-lg">Front End Developer</h2>
-        <p className="text-center my-3 text-sm">
-          Empathic creative wielding the web <span className="relative color-line">for good</span>. A Seattle native, when not coding you&apos;ll find me cycling toward sunshine.
+        <p className="text-center my-3 sm:mx-16 text-sm">
+          Empathic creative wielding the web for good. A Seattle native, when not coding you&apos;ll find me cycling toward sunshine.
         </p>
         <div className="flex flex-row gap-2 mt-5">
           <div className="flex flex-col items-center group relative">
             <Link href="/resume.pdf">
-              <a className="z-10 hover:text-gray-500 color-line flex flex-row items-center gap-2">
+              <a className="z-10 active:scale-90 transition-all hover:text-gray-600 color-line flex flex-row items-center gap-2">
                 <FontAwesomeIcon color="#C65454" size="lg" icon={solid("file-pdf")} />
                 Download resume
               </a>
@@ -41,60 +65,52 @@ export default function Home() {
         <main className="mt-16">
           <div>
             <h2 className="font-black text-xl">Recent work</h2>
-            <p>Two of my favorite recent projects with live site and source code</p>
-
-            <div className="card border-black mt-5 flex flex-row gap-5 relative">
-              <div className="w-20 h-20 aspect-square grid place-content-center self-center bg-gradient-to-b from-[#C480F9] to-[#A630DE] p-3 rounded-2xl text-purple-50">
-                <FontAwesomeIcon size="3x" icon={solid('compact-disc')} />
-              </div>
-              <div className="flex flex-col">
-                <h3 className="text-xl">Streamy</h3>
-                <p className="text-xs">Subscriber exclusive listening serving thousands of users</p>
-                <p className="font-semibold text-xs mt-2">
-                  Built with React / NextJs / Redux / Firebase
-                </p>
-                <div className="self-end relative flex flex-row gap-2 mt-2">
-                  <Icon iconStyles="hover:text-purple-primary" text="Live site" icon={<FontAwesomeIcon icon={solid('desktop')} />} />
-                  <Icon iconStyles="hover:text-purple-primary" text="Source code" icon={<FontAwesomeIcon size="lg" icon={brands('github')} />} />
-                </div>
-              </div>
-            </div>
-
-            <div className="card border-blue-primary mt-5 flex flex-row gap-5">
-              <div className="self-center">
-                <svg width="82" height="82" viewBox="0 0 82 82" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <rect width="82" height="82" rx="19" fill="url(#paint0_linear_0_2)" />
-                  <path d="M41 64.119V19.3155" stroke="white" strokeWidth="4" />
-                  <path d="M53.9524 29.6545H28.0476V55.5019H53.9524V29.6545Z" stroke="white" strokeWidth="4" />
-                  <defs>
-                    <linearGradient id="paint0_linear_0_2" x1="41" y1="0" x2="41" y2="82" gradientUnits="userSpaceOnUse">
-                      <stop stopColor="#3BA0FD" />
-                      <stop offset="1" stopColor="#2084E1" />
-                    </linearGradient>
-                  </defs>
-                </svg>
-
-              </div>
-              <div>
-                <h3 className="text-xl">Sound Splits</h3>
-                <p className="text-xs">Royalty accounting SaaS for labels and publishers</p>
-                <p className="font-semibold text-xs mt-2">
-                  Built with Tailwind / React / NextJs / Redux / Django
-                </p>
-              </div>
-            </div>
+            <p>Two of my favorite recent projects with live site and source.</p>
+          <Card 
+            icon={<div className="w-20 h-20 aspect-square grid place-content-center self-start mt-1 bg-gradient-to-b from-[#C480F9] to-[#A630DE] p-3 rounded-2xl text-purple-50">
+            <FontAwesomeIcon size="3x" icon={solid('compact-disc')} />
+                  </div>}
+            title="Streamy" 
+            desc="Subscriber exclusive listening serving thousands of users" 
+            stack="Built with React / NextJs / Redux / Firebase"
+            links={{livesite: '#', github:'#'}} 
+            iconStyles="hover:text-purple-primary"
+            borderColor="border-purple-primary"
+          />
+          <Card 
+            icon={<div className="self-start mt-1">
+            <svg width="82" height="82" viewBox="0 0 82 82" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <rect width="82" height="82" rx="19" fill="url(#paint0_linear_0_2)" />
+              <path d="M41 64.119V19.3155" stroke="white" strokeWidth="4" />
+              <path d="M53.9524 29.6545H28.0476V55.5019H53.9524V29.6545Z" stroke="white" strokeWidth="4" />
+              <defs>
+                <linearGradient id="paint0_linear_0_2" x1="41" y1="0" x2="41" y2="82" gradientUnits="userSpaceOnUse">
+                  <stop stopColor="#3BA0FD" />
+                  <stop offset="1" stopColor="#2084E1" />
+                </linearGradient>
+              </defs>
+            </svg>
+          </div>}
+            title="Sound Splits" 
+            desc="Royalty accounting SaaS for labels and publishers" 
+            stack="Built with Tailwind / React / NextJs / Redux / Django"
+            links={{livesite: '#', github:'#'}} 
+            iconStyles="hover:text-blue-primary"
+            borderColor="border-blue-primary"
+            />
+            <p className='text-center text-sm mt-6'>Visit my <Link href="https://github.com/xyeres"><a className='border-b-4 border-b-gray-300 hover:border-b-gray-500 transition-all'>GitHub profile</a></Link> for more samples.</p>
           </div>
-          <div className="flex flex-col mt-16">
+          <div className="flex flex-col mt-12">
             <h2 className="font-black text-xl">Resume</h2>
-            <p>Grab a copy of my resume, and feel free to share it</p>
-            <button className="mt-3 flex justify-center items-center flex-row gap-3 hover:border-b-0 hover:mt-4 transition-all border-b-4 border-b-blue-shadow bg-blue-secondary hover:bg-blue-shadow rounded-md text-white py-3 px-8">
-              <FontAwesomeIcon icon={solid('file-pdf')} size="lg" />
+            <p className='sm:mb-6'>Grab a copy of my resume, and feel free to share it</p>
+            <button className="mt-3 mx-10 flex justify-center items-center flex-row gap-3 hover:scale-95 active:border-b-0 active:mt-4 transition-all border-b-4 border-b-blue-shadow hover:border-b-blue-900 bg-blue-primary hover:bg-blue-shadow rounded-md text-white py-3 px-8">
+              <FontAwesomeIcon  icon={solid('file-pdf')} size="lg" />
               Download resume
             </button>
           </div>
           <div className="flex flex-col mt-16">
             <h2 className="font-black text-xl">Contact</h2>
-            <p>I&apos;d love to hear what you are working on, send me something interesting!</p>
+            <p className='mb-5'>I&apos;d love to hear what you are working on, send me something interesting!</p>
             <form method="post" className="w-full flex flex-col" action="/">
               <div className="input-control mt-2">
                 <label htmlFor="name">Name</label>
@@ -109,7 +125,7 @@ export default function Home() {
                 <textarea rows="3" name="message" />
               </div>
               <button className="
-                mt-2 flex justify-center items-center flex-row gap-3 hover:border-b-0 hover:mt-3 transition-all border-b-4 border-b-blue-shadow bg-blue-secondary hover:bg-blue-shadow rounded-md text-white py-3 px-8 ml-36
+                mt-3 hover:scale-95 flex justify-center items-center flex-row gap-3 active:border-b-0 active:mt-4 transition-all border-b-4 border-b-blue-shadow hover:border-b-blue-900 bg-blue-secondary hover:bg-blue-shadow rounded-md text-white py-3 px-8 w-42 ml-auto
               ">
                 <FontAwesomeIcon icon={solid('paper-plane')} size="sm" />
                 Send
@@ -121,6 +137,6 @@ export default function Home() {
           <p className="text-xs mt-2 text-slate-400">&copy;2022 Michael Carr</p>
         </footer>
       </div>
-    </>
+    </div>
   )
 }
